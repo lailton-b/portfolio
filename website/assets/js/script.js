61b9c43f2b23195fdbb0acd9cfee6ea0ed62b10e
SensiPhi <lailtoneletrohits@hotmail.com>
2020-06-23 12:35:40 -0300

    Portfolio update

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -9,3 +9,30 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("open-menu");
   introduction.classList.toggle("open-menu");
 });
+
+/* Form */
+
+const name = document.querySelector("#nome");
+const nameParent = document.querySelector(".nome");
+const email = document.querySelector("#email");
+const emailParent = document.querySelector(".email");
+
+function handleName() {
+  if (name.value.length > 0) {
+    nameParent.classList.remove("invalid");
+  } else {
+    nameParent.classList.add("invalid");
+  }
+}
+
+function handleEmail() {
+  console.log("a");
+  if (email.checkValidity()) {
+    emailParent.classList.remove("invalid");
+  } else {
+    emailParent.classList.add("invalid");
+  }
+}
+
+name.addEventListener("keyup", handleName);
+email.addEventListener("keyup", handleEmail);
