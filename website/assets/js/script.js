c99383505b6ab9e3bb9645ec1aec398b1e8e9d13
SensiPhi <lailtoneletrohits@hotmail.com>
2020-08-02 12:11:34 -0300

    Form validation update

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -1,38 +1,53 @@
 /* Menu Hamburger */
 
-const menu = document.querySelector(".menu__menu_hamburger");
-const introduction = document.querySelector(".introduction");
+const menu = document.querySelector('.menu__menu_hamburger');
+const introduction = document.querySelector('.introduction');
 
-menu.addEventListener("click", () => {
-  menu.classList.remove("no-animation");
-  menu.classList.toggle("close-menu");
-  menu.classList.toggle("open-menu");
-  introduction.classList.toggle("open-menu");
+menu.addEventListener('click', () => {
+  menu.classList.remove('no-animation');
+  menu.classList.toggle('close-menu');
+  menu.classList.toggle('open-menu');
+  introduction.classList.toggle('open-menu');
 });
 
 /* Form */
 
-const name = document.querySelector("#nome");
-const nameParent = document.querySelector(".nome");
-const email = document.querySelector("#email");
-const emailParent = document.querySelector(".email");
+// Name validation
+
+const name = document.querySelector('#nome');
+const nameParent = document.querySelector('.nome');
 
 function handleName() {
   if (name.value.length > 0) {
-    nameParent.classList.remove("invalid");
+    nameParent.classList.remove('invalid');
   } else {
-    nameParent.classList.add("invalid");
+    nameParent.classList.add('invalid');
   }
 }
 
-function handleEmail() {
-  console.log("a");
-  if (email.checkValidity()) {
-    emailParent.classList.remove("invalid");
-  } else {
-    emailParent.classList.add("invalid");
+name.addEventListener('keyup', handleName);
+
+// E-mail validation
+
+const email = document.querySelector('#email');
+const emailParent = document.querySelector('.email');
+let emailError = false;
+
+function handleEmailBlur() {
+  if (!email.checkValidity()) {
+    emailParent.classList.add('invalid');
+    emailError = true;
   }
 }
 
-name.addEventListener("keyup", handleName);
-email.addEventListener("keyup", handleEmail);
+function handleEmailKey() {
+  if (emailError) {
+    if (email.checkValidity()) {
+      emailParent.classList.remove('invalid');
+      emailError = false;
+    }
+  } else return;
+}
+
+email.addEventListener('keyup', handleEmailKey);
+email.addEventListener('blur', handleEmailBlur);
